434d977e5d3c885ad272e21ddde91a8dca0d1533
krishesak <krishesak@gmail.com>
2020-01-31 23:45:40 +0530

    Update test_data_store.rb

--- test_data_store.rb
+++ test_data_store.rb
@@ -4,7 +4,18 @@ require 'minitest/autorun'
 describe DataStore do
   
   before(:all) do
-  	@data_store = DataStore.new
+    @data_store = DataStore.new
   end
-	
+
+  describe "create data" do
+    it "when creating with time-to-live value" do
+      response = @data_store.create("Employee", {"name"=>"Esakki"}, 40)
+      response.must_equal "Successfully created the data"
+    end
+
+    it "when creating without time-to-live value" do
+      response = @data_store.create("Vehicle", {"name"=>"RE"})
+      response.must_equal "Successfully created the data"
+    end
+  end
 end
