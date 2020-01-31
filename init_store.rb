dfaefc4262a5b5cf998c9a4b7ad7ffcd3fda189f
krishesak <krishesak@gmail.com>
2020-01-31 18:35:29 +0530

    Add files via upload

--- init_store.rb
+++ init_store.rb
@@ -8,23 +8,26 @@ data_store1 = DataStore.new("sample_test_store")
 # initialize data store in default location
 p "initialising data store in default location.."
 data_store2 = DataStore.new
-
+threads = []
 # it creates a key-value pair with no time-to-live property.
-data_store1.create("Employee", {"name"=>"Esakki","Age"=> "TCS", "phone"=>"9597270720"})
+threads << Thread.new{ data_store1.create("Employee", {"name"=>"Esakki","Age"=> "TCS", "phone"=>"9597270720"}) }
 
-data_store2.create("Company", {"name"=>"Tcs","location"=> "chennai", "phone"=>"5455454"})
+threads << Thread.new{ data_store2.create("Company", {"name"=>"Tcs","location"=> "chennai", "phone"=>"5455454"}) }
 
 # it creates a key-value pair with time-to-live property.
-data_store1.create("Vehicle", {"name"=>"R15","prize"=> "200000"}, 60)
+threads << Thread.new{ data_store1.create("Vehicle", {"name"=>"R15","prize"=> "200000"}, 60) }
 
-data_store2.create("Owner", {"name"=>"veera","location"=> "chennai"}, 45)
+threads << Thread.new{ data_store2.create("Owner", {"name"=>"veera","location"=> "chennai"}, 45) }
 # it returns the value of a respective key in json format if key has not expired
 # otherwise it will raise an error
-data_store1.read("Employee")
+threads << Thread.new{ data_store1.read("Employee") }
 
-data_store2.read("Owner")
+threads << Thread.new{ data_store2.read("Owner") }
 # it deletes the respective key from the data store if key has not expired
 # otherwise it will raise an error
-data_store1.delete("Vehicle")
+threads << Thread.new{ data_store1.delete("Vehicle") }
 
-data_store2.delete("Company")
+threads << Thread.new{ data_store2.delete("Company") }
+
+# Use 'join' for main thread to be waiting for the child threads to be finished.
+threads.map(&:join)
